36728b6ea69888e5a43824548b2ffd640c5e93b0
nidhiraykhere <nidhiraykhere838@gmail.com>
2024-04-22 23:11:53 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,7 @@
+// var btn = document.querySelector("#right3 button")
+// btn.addEventListener('mouseenter',function(){
+//     btn.style.padding= "2.2px 14px"
+// })
+// btn.addEventListener('mouseleave',function(){
+//     btn.style.padding= "2.2px 10px"
+// })
